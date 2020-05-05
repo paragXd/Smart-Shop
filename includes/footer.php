2703c9114c7d1e2e5ca1210530d872c389d17106
Parag <rckngprg@gmail.com>
2020-05-06 02:14:15 +0530

    first build

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container-fluid">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2019 Smart Shop</strong>
+    </div>
+</footer>
